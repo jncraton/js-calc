648be2ba9e9ffea379e24f5b1a929b2ab5145c1a
Jon Craton <jncraton@gmail.com>
2025-10-30 12:40:51 -0400

    Add test_index.py

---
+++ test_index.py
@@ -0,0 +1,28 @@
+import re
+from playwright.sync_api import Page, expect
+import pytest
+
+from pathlib import Path
+
+file_url = Path("index-impl.html").resolve().as_uri()
+
+@pytest.fixture
+def index(page: Page):
+    page.goto(f"{file_url}")
+
+    return page
+
+def test_add(index):
+    index.get_by_text("1").click()
+    index.get_by_text("2").click()
+    index.get_by_text("3").click()
+    index.get_by_text("4").click()
+    index.get_by_text("5").click()
+    index.get_by_text("+").click()
+    index.get_by_text("6").click()
+    index.get_by_text("7").click()
+    index.get_by_text("8").click()
+    index.get_by_text("9").click()
+    index.get_by_text("0").click()
+    index.get_by_text("=").click()
+    expect(index.get_by_text("80235")).to_be_visible()
