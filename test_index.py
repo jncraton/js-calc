56fa8f9f884bedad137f703b4d2ad92bd3182fec
Jon Craton <jncraton@gmail.com>
2025-10-30 12:54:03 -0400

    Add whitespace

--- test_index.py
+++ test_index.py
@@ -6,11 +6,13 @@ from pathlib import Path
 
 file_url = Path("index.html").resolve().as_uri()
 
+
 @pytest.fixture
 def index(page: Page):
     page.goto(f"{file_url}")
 
     return page
+
 
 def test_add(index):
     index.get_by_text("1").click()
@@ -27,6 +29,7 @@ def test_add(index):
     index.get_by_text("=").click()
     expect(index.get_by_text("80235")).to_be_visible()
 
+
 def test_sub(index):
     index.get_by_text("9").click()
     index.get_by_text("0").click()
@@ -35,12 +38,14 @@ def test_sub(index):
     index.get_by_text("=").click()
     expect(index.get_by_text("85")).to_be_visible()
 
+
 def test_mul(index):
     index.get_by_text("8").click()
     index.get_by_text("x").click()
     index.get_by_text("7").click()
     index.get_by_text("=").click()
     expect(index.get_by_text("56")).to_be_visible()
+
 
 def test_div(index):
     index.get_by_text("1").click()
@@ -51,12 +56,14 @@ def test_div(index):
     index.get_by_text("=").click()
     expect(index.get_by_text("32")).to_be_visible()
 
+
 def test_div_frac(index):
     index.get_by_text("8").click()
     index.get_by_text("/").click()
     index.get_by_text("5").click()
     index.get_by_text("=").click()
     expect(index.get_by_text("1.6")).to_be_visible()
+
 
 def test_decimal(index):
     index.get_by_text("1").click()
